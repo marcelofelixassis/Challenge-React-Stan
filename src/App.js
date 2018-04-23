696dc7f71fd6376b978bd035090f87489a9f003e
Marcelo Félix <marcelofelix.af@gmail.com>
2018-04-23 13:51:19 -0300

    footer js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,41 @@ import logo from './logo.svg';
 
 class App extends Component {
   render() {
+
+    window.onload = function() {
+      var topo  = document.querySelector(".content__header");
+      var conteudo = document.querySelector("#content__movies");
+      var rodape = document.querySelector(".content__footer");
+      var tamanhoTela = window.innerHeight;
+
+      if (rodape.offsetTop < tamanhoTela - rodape.offsetHeight) {
+        conteudo.style.marginBottom = (tamanhoTela - topo.offsetHeight - conteudo.offsetHeight - rodape.offsetHeight - 35) + "px";
+      } else {
+        conteudo.style.marginBottom = "20px";
+      }
+    }
+
+    window.onresize = function() {
+      var topo  = document.querySelector(".content__header");
+      var conteudo = document.querySelector("#content__movies");
+      var rodape = document.querySelector(".content__footer");
+      var tamanhoTela = window.innerHeight;
+
+
+      console.log(tamanhoTela);
+      console.log(tamanhoTela);
+      console.log(tamanhoTela);
+      console.log(tamanhoTela);
+      
+      if (rodape.offsetTop < tamanhoTela - rodape.offsetHeight) {
+        conteudo.style.marginBottom = (tamanhoTela - topo.offsetHeight - conteudo.offsetHeight - rodape.offsetHeight - 35) + "px";
+      } else {
+        conteudo.style.marginBottom = "20px";
+      }
+    }
+ 
     return (
+      <div>
       <div className="App">
         <Header />
         <div className="grid" id="content__movies">
@@ -16,9 +50,11 @@ class App extends Component {
             <Movie type="SERIES" sub_text="Popular Series"/>
             <Movie type="MOVIES" sub_text="Popular Movies"/>
             <Movie type="SERIES" sub_text="Popular Series"/>
-            <Movie type="MOVIES" sub_text="Popular Movies"/>
           </div>
         </div>
+        
+      </div>  
+      <footer className="content__footer">asdfasdf</footer>  
       </div>
     );
   }
